3d364159ec7bba013028e2d1ee877cac9d60e27b
Josh Fraley <fraley.j3@gmail.com>
2023-04-05 21:24:57 -0500

    class activity, 4 Apr

---
+++ WEEK 3/3.2.1-Arrays.js
@@ -0,0 +1,18 @@
+// create an Array using an Array literal
+const savvy = ["David", "Steph", "Matt",];
+
+// access the 1st item in the Array
+console.log(savvy[0]);
+
+// access the last item in the Array
+console.log(savvy[savvy.length-1]);
+
+// print the length of the Array
+console.log(savvy.length);
+
+// use the length property to access the last item in the Array
+console.log(savvy[savvy.length-1]);
+
+// with for...of, loop over the Array, modify the value and add to a different Array
+  for(let sav of savvy){
+};
